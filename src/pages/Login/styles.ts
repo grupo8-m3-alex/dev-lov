2d65c7074e31c9cbba20cdb15a0b08cb90dcf0bc
Rodrigo Nunes <imrodrigon@gmail.com>
2022-08-31 09:14:37 -0400

    feat: login

---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,127 @@
+import styled from 'styled-components';
+
+interface Props {
+  isFocused: boolean;
+}
+
+export const Container = styled.div`
+  background-image: linear-gradient(
+    to bottom,
+    #f20171,
+    #b01956,
+    #731b3c,
+    #3b1521,
+    #000000
+  );
+  min-height: 100vh;
+  & > div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    gap: 2.375rem;
+
+    min-height: 100vh;
+  }
+`;
+
+export const Form = styled.form`
+  width: 100%;
+  max-width: 28.4375rem;
+  padding: 0 1.4375rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 26px;
+
+  h1 {
+    font-weight: 600;
+    font-size: 2.25rem;
+    text-align: center;
+  }
+
+  h3 {
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+  }
+  label {
+    color: #ffffff;
+    font-size: 0.875rem;
+    font-weight: 600;
+  }
+
+  .emailInput {
+    display: flex;
+    flex-direction: column;
+    gap: 1.0625rem;
+
+    input {
+      width: 100%;
+      padding: 1.0625rem 0.9375rem;
+      border-radius: 20px;
+      border: none;
+      outline: none;
+
+      color: rgba(255, 255, 255, 0.6);
+      font-size: 1.25rem;
+      font-weight: 600;
+      background-color: rgba(255, 255, 255, 0.6);
+
+      &:focus {
+        border: 2px solid #ffffff;
+      }
+    }
+  }
+
+  .passwordError {
+    color: rgb(255, 255, 0);
+  }
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  background-color: #f20171;
+  font-size: 20px;
+  font-weight: 600;
+  border: none;
+  padding: 1.125rem;
+  border-radius: 1rem;
+
+  &:hover {
+    background-color: #ffffff;
+    color: #f20171;
+
+    cursor: pointer;
+  }
+`;
+
+export const Password = styled.div<Props>`
+  display: flex;
+  align-items: center;
+
+  margin-bottom: 10px;
+
+  border: 2px solid ${({ isFocused }) => (isFocused ? '#FFF' : 'none')};
+  padding: 0 0.4375rem;
+  border-radius: 20px;
+  height: 50px;
+
+  color: white;
+
+  background-color: rgba(255, 255, 255, 0.51);
+
+  input {
+    background-color: transparent;
+    width: 100%;
+    border: none;
+    font-size: 1.25rem;
+    outline: none;
+  }
+
+  & > div {
+    display: flex;
+    align-items: center;
+  }
+`;
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,47 +1,66 @@
 import axios from 'axios';
 import { FormDataDefault } from '../components/Input';
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: 'https://json-server-apikenzie.herokuapp.com/',
-  timeout: 5000
+  timeout: 5000,
 });
 
 export const singIn = async (data) => {
-  return await
-    api
-      .post('login', data)
-      .then(({ data }) => {
-        localStorage.setItem("@token_devlov", data.accessToken);
-      })
-      .catch(err => console.error(err))
-}
+  await api
+    .post('login', data)
+    .then(({ data }) => {
+      localStorage.setItem('@token_devlov', data.accessToken);
+    })
+    .catch((err) => console.error(err));
+};
 
 export const userIsValid = async (token, user) => {
-  api.defaults.headers.common["Authorization"] = token
-  return await
-    api
-      .get(`users/${user.id}`)
-      .then(res => {
-        console.log(res.data);
-        return true
-      })
-      .catch(err => {
-        console.error(err);
-        return false
-      })
-}
+  api.defaults.headers.common['Authorization'] = token;
+  return await api
+    .get(`users/${user.id}`)
+    .then((res) => {
+      console.log(res.data);
+      return true;
+    })
+    .catch((err) => {
+      console.error(err);
+      return false;
+    });
+};
 
 export const registerUser = async (data: FormDataDefault) => {
-  const { name, email, url_avatar, password, confirmPassword, age, bio, city, state, gender } = data
-  return await
-    api
-      .post('register', { name, email, url_avatar, password, confirmPassword, age, bio, city, state, gender })
-      .then(res => {
-        console.log(res);
-        return true;
-      })
-      .catch(err => {
-        console.log(err);
-        return false;
-      })
-}
+  const {
+    name,
+    email,
+    url_avatar,
+    password,
+    confirmPassword,
+    age,
+    bio,
+    city,
+    state,
+    gender,
+  } = data;
+  return await api
+    .post('register', {
+      name,
+      email,
+      url_avatar,
+      password,
+      confirmPassword,
+      age,
+      bio,
+      city,
+      state,
+      gender,
+    })
+    .then((res) => {
+      console.log(res);
+      return true;
+    })
+    .catch((err) => {
+      console.log(err);
+      return false;
+    });
+};
